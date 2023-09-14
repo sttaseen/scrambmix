7279b47534e21ce78550999f82908d4cd747204c
Sadat <sttaseen@github.com>
2023-09-14 19:40:42 +1200

    misc: add configs for train

--- experiments/scrambmix/automate.sh
+++ experiments/scrambmix/automate.sh
@@ -1,16 +1,10 @@
 cd ../..
 
-# python mmaction2/tools/train.py experiments/scrambmix/configs/alpha=1.py --validate --deterministic --seed 0
+# Rented GPU
+python mmaction2/tools/train.py experiments/scrambmix/configs/alpha=5.py --validate --deterministic --seed 0
 
-# python mmaction2/tools/train.py experiments/scrambmix/configs/alpha=0.1.py --validate --deterministic --seed 0
-
-python mmaction2/tools/train.py experiments/scrambmix/configs/alpha=0.5-1.py --validate --deterministic --seed 0
-
-# TODO: Do these later.
-"""
 python mmaction2/tools/train.py experiments/scrambmix/configs/alpha=2.py --validate --deterministic --seed 0
 
-python mmaction2/tools/train.py experiments/scrambmix/configs/alpha=0.1.py --validate --deterministic --seed 0
+# python mmaction2/tools/train.py experiments/scrambmix/configs/alpha=0.2.py --validate --deterministic --seed 0
 
-python mmaction2/tools/train.py experiments/scrambmix/configs/alpha=1.py --validate --deterministic --seed 0
-"""
+# python mmaction2/tools/train.py experiments/scrambmix/configs/alpha=1.py --validate --deterministic --seed 0

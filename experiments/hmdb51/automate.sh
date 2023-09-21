c24bf87a5b76948b69a16ccb9aa21aa942de9f0c
Sadat <sttaseen@github.com>
2023-09-21 17:07:06 +1200

    feat: add test hmdb

---
+++ experiments/hmdb51/automate.sh
@@ -0,0 +1,9 @@
+cd ../..
+
+# Grid Search
+
+python mmaction2/tools/train.py experiments/hmdb51/slow-fast/baseline.py --validate --deterministic --seed 0
+
+
+
+
--- experiments/hmdb51/sadat_automate.sh
+++
@@ -1,7 +0,0 @@
-cd ../..
-
-# Sadat
-
-python mmaction2/tools/train.py experiments/hmdb51/cutmix.py --validate --deterministic --seed 0
-
-python mmaction2/tools/train.py experiments/hmdb51/mixup.py --validate --deterministic --seed 0

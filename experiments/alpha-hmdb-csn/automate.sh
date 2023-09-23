4da2b012cd4e7d2d24070bd5ab755b718953fd0b
Sadat <sttaseen@github.com>
2023-09-24 10:46:36 +1300

    feat: add alpha wlasl

--- experiments/alpha-hmdb-csn/automate.sh
+++ experiments/alpha-hmdb-csn/automate.sh
@@ -2,8 +2,10 @@ cd ../..
 
 # Grid Search?
 
+python mmaction2/tools/train.py experiments/alpha/baseline.py --validate --deterministic --seed 0
+
 # python mmaction2/tools/train.py experiments/alpha/alpha=0.1.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha/alpha=0.25.py --validate --deterministic --seed 0
+# python mmaction2/tools/train.py experiments/alpha/alpha=0.25.py --validate --deterministic --seed 0
 # python mmaction2/tools/train.py experiments/alpha/alpha=0.5.py --validate --deterministic --seed 0
 # python mmaction2/tools/train.py experiments/alpha/alpha=1.py --validate --deterministic --seed 0
 # python mmaction2/tools/train.py experiments/alpha/alpha=2.py --validate --deterministic --seed 0

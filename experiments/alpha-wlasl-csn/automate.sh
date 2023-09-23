ee1b4260a72550335eae1f4393dc44206f692da9
Sadat <sttaseen@github.com>
2023-09-24 10:50:03 +1300

    misc: add alpha automate

--- experiments/alpha-wlasl-csn/automate.sh
+++ experiments/alpha-wlasl-csn/automate.sh
@@ -4,12 +4,12 @@ cd ../..
 
 python mmaction2/tools/train.py experiments/alpha-hmdb-csn/baseline.py --validate --deterministic --seed 0
 
-# python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=0.1.py --validate --deterministic --seed 0
-# python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=0.25.py --validate --deterministic --seed 0
-# python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=0.5.py --validate --deterministic --seed 0
-# python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=1.py --validate --deterministic --seed 0
-# python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=2.py --validate --deterministic --seed 0
-# python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=4.py --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=0.1.py --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=0.25.py --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=0.5.py --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=1.py --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=2.py --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=4.py --validate --deterministic --seed 0
 
 
 

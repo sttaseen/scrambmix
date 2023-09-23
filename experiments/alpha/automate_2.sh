d92f58b52c1273f674e7e80bb93401eb16506f2f
Sadat <sttaseen@github.com>
2023-09-23 13:03:59 +1200

    misc: add automate_2 and automate_3 alpha exp

--- experiments/alpha/automate_2.sh
+++ experiments/alpha/automate_2.sh
@@ -3,9 +3,9 @@ cd ../..
 # Grid Search?
 
 # python mmaction2/tools/train.py experiments/alpha/alpha=0.1.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha/alpha=0.25.py --validate --deterministic --seed 0
-# python mmaction2/tools/train.py experiments/alpha/alpha=0.5.py --validate --deterministic --seed 0
-# python mmaction2/tools/train.py experiments/alpha/alpha=1.py --validate --deterministic --seed 0
+# python mmaction2/tools/train.py experiments/alpha/alpha=0.25.py --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha/alpha=0.5.py --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha/alpha=1.py --validate --deterministic --seed 0
 # python mmaction2/tools/train.py experiments/alpha/alpha=2.py --validate --deterministic --seed 0
 # python mmaction2/tools/train.py experiments/alpha/alpha=4.py --validate --deterministic --seed 0
 

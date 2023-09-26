d47a49a37f3c4f81886b88566d405d4bc0c2326c
Sadat <sttaseen@github.com>
2023-09-26 18:20:08 +1300

    misc: change lr settings

--- experiments/alpha-wlasl-csn/alpha=0.5.py
+++ experiments/alpha-wlasl-csn/alpha=0.5.py
@@ -185,12 +185,12 @@ optimizer = dict(type='SGD', lr=0.000125, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 lr_config = dict(
     policy='step',
-    step=[290],
+    step=[290, 450],
     warmup='linear',
     warmup_ratio=0.1,
     warmup_by_epoch=True,
     warmup_iters=16)
-total_epochs =400
+total_epochs =500
 work_dir = './work_dirs/a=0.5/'
 find_unused_parameters = True
 omnisource = False

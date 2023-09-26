12138f20f8150b80d56917d4f9a5ef9025b71050
Sadat <sttaseen@github.com>
2023-09-26 15:17:39 +1300

    misc: increase epoch alpha wlasl

--- experiments/alpha-wlasl-csn/automate.sh
+++ experiments/alpha-wlasl-csn/automate.sh
@@ -1,15 +1,13 @@
 cd ../..
 
 # Grid Search?
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=0.1.py --resume work_dirs/a\=0.1/latest.pth --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=0.25.py --resume work_dirs/a\=0.25/latest.pth --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=0.5.py --resume work_dirs/a\=0.5/latest.pth --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=1.py --resume work_dirs/a\=1/latest.pth --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=2.py --resume work_dirs/a\=2/latest.pth --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=4.py --resume work_dirs/a\=4/latest.pth --validate --deterministic --seed 0
 
-python mmaction2/tools/train.py experiments/alpha-hmdb-csn/baseline.py --validate --deterministic --seed 0
-
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=0.1.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=0.25.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=0.5.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=1.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=2.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha=4.py --validate --deterministic --seed 0
 
 
 

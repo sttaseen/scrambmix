e555320aa1c24cb52383c9f778c3f67cf570b886
Sadat <sttaseen@github.com>
2023-09-28 14:15:57 +1300

    bug: fix automate alpha

--- experiments/alpha-wlasl-reversed-scrambmix/automate.sh
+++ experiments/alpha-wlasl-reversed-scrambmix/automate.sh
@@ -1,12 +1,12 @@
 cd ../..
 
 # Grid Search?
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=0.1.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=0.25.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=0.5.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=1.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=2.py --validate --deterministic --seed 0
-python mmaction2/tools/train.py experiments/alpha-wlasl-csn/alpha\=4.py --validate --deterministic --seed 0
+python mmaction2/tools/train.py experiments/alpha-wlasl-reversed-scrambmix/alpha\=0.1.py --validate --deterministic --seed 0
+# python mmaction2/tools/train.py experiments/alpha-wlasl-reversed-scrambmix/alpha\=0.25.py --validate --deterministic --seed 0
+# python mmaction2/tools/train.py experiments/alpha-wlasl-reversed-scrambmix/alpha\=0.5.py --validate --deterministic --seed 0
+# python mmaction2/tools/train.py experiments/alpha-wlasl-reversed-scrambmix/alpha\=1.py --validate --deterministic --seed 0
+# python mmaction2/tools/train.py experiments/alpha-wlasl-reversed-scrambmix/alpha\=2.py --validate --deterministic --seed 0
+# python mmaction2/tools/train.py experiments/alpha-wlasl-reversed-scrambmix/alpha\=4.py --validate --deterministic --seed 0
 
 
 

c02a5acea5595c545f5a566d3df64351962877c6
Sadat <sttaseen@github.com>
2023-10-01 11:19:13 +1300

    misc: decrease workers sv2

--- experiments/something-something-v2/baseline.py
+++ experiments/something-something-v2/baseline.py
@@ -109,7 +109,7 @@ test_pipeline = [
 ]
 data = dict(
     videos_per_gpu=32,
-    workers_per_gpu=16,
+    workers_per_gpu=4,
     test_dataloader=dict(videos_per_gpu=1),
     val_dataloader=dict(videos_per_gpu=1),
     train=dict(

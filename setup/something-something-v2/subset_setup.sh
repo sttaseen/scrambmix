81f8686fb604a8ca2e0079a90b16371ff75c89bb
Sadat <sttaseen@github.com>
2023-10-07 14:34:48 +1300

    bug: fix json file being deleted

--- setup/something-something-v2/subset_setup.sh
+++ setup/something-something-v2/subset_setup.sh
@@ -16,9 +16,9 @@ unzip something-something-v2.zip -d ./
 cd ../..
 
 # Move the subset JSON files and replace the originals
-mv tools/something-something-v2/subset_train_labels.json data/something-something-v2/something-something-v2/train_labels.json
-mv tools/something-something-v2/subset_test_labels.json data/something-something-v2/something-something-v2/test_labels.json
-mv tools/something-something-v2/subset_val_labels.json data/something-something-v2/something-something-v2/val_labels.json
+cp tools/something-something-v2/subset_train_labels.json data/something-something-v2/something-something-v2/train_labels.json
+cp tools/something-something-v2/subset_test_labels.json data/something-something-v2/something-something-v2/test_labels.json
+cp tools/something-something-v2/subset_val_labels.json data/something-something-v2/something-something-v2/val_labels.json
 
 # Split the videos
 python tools/something-something-v2/split_videos.py -d data/something-something-v2/something-something-v2

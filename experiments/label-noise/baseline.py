b9e29f4b5ce5523178b1c0dfa40e725e2fbf017a
Sadat <sttaseen@github.com>
2023-10-07 19:32:53 +1300

    misc: adjust data per gpu

--- experiments/label-noise/baseline.py
+++ experiments/label-noise/baseline.py
@@ -108,7 +108,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['imgs'])
 ]
 data = dict(
-    videos_per_gpu=32,
+    videos_per_gpu=8,
     workers_per_gpu=4,
     test_dataloader=dict(videos_per_gpu=1),
     val_dataloader=dict(videos_per_gpu=1),

cbf6821d55232e9a00d0e70ff7f260fddd082b56
Sadat <sttaseen@github.com>
2023-10-11 10:52:37 +1300

    misc: separate autsl trains

---
+++ experiments/autsl/automate_1.sh
@@ -0,0 +1,9 @@
+cd ../..
+
+python mmaction2/tools/train.py experiments/autsl/fframecutmix.py --validate --deterministic --seed 0
+
+# python mmaction2/tools/train.py experiments/autsl/framecutmix.py --validate --deterministic --seed 0
+
+# python mmaction2/tools/train.py experiments/autsl/scrambmix.py --validate --deterministic --seed 0
+
+

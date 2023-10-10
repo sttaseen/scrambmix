b150320ac516cf57862606370ef3d0844ee2d3a4
Sadat <sttaseen@github.com>
2023-10-11 10:40:28 +1300

    feat: add wlasl leftover experiments

--- experiments/wlasl/automate_1.sh
+++
@@ -1,9 +0,0 @@
-cd ../..
-
-# GPU 1
-
-python mmaction2/tools/train.py experiments/wlasl/mixup.py --validate --deterministic --seed 0
-
-python mmaction2/tools/train.py experiments/wlasl/cutmix.py --validate --deterministic --seed 0
-
-
--- experiments/wlasl/automate_2.sh
+++
@@ -1,9 +0,0 @@
-cd ../..
-
-# GPU 2
-
-python mmaction2/tools/train.py experiments/wlasl/baseline.py --validate --deterministic --seed 0
-
-python mmaction2/tools/train.py experiments/wlasl/scrambmix.py --validate --deterministic --seed 0
-
-
